3629e6d5774ba04d7e2a427e32cb0cfd4cfd6a4a
Jacob Winch <winch@ualberta.ca>
2023-12-06 20:24:35 -0700

    Few minor changes

--- code/methodology/residual_analysis.R
+++ code/methodology/residual_analysis.R
@@ -24,44 +24,47 @@ t <- rstudent(model)
 yhat <- model$fit
 
 
-qqnorm(t, col="blue")
-qqline(t, col=2)
 
-plot(yhat, t, col="blue", xlab = "Fitted Values", ylab = "Residuals", main = "Residuals vs Fitted Values")
-abline(h=0, col="red")
+# qqnorm(t, col="blue")
+# qqline(t, col=2)
+
+# plot(yhat, t, col="blue", xlab = "Fitted Values", ylab = "Residuals", main = "Residuals vs Fitted Values")
+# abline(h=0, col="red")
+ 
+#windows()
+par(mfrow=c(2,2))
 
 # Plots of Residuals versus the Regressors
-
-plot(X1, t, col = "blue", xlab = "x1", ylab = "Residuals", main = "Residuals vs x1")
-abline(h=0, col = "red")
-plot(X2, t, col = "blue", xlab = "x2", ylab = "Residuals", main = "Residuals vs x2")
-abline(h=0, col = "red")
-plot(X3, t, col = "blue", xlab = "x3", ylab = "Residuals", main = "Residuals vs x3")
-abline(h=0, col = "red")
-plot(X4, t, col = "blue", xlab = "x4", ylab = "Residuals", main = "Residuals vs x4")
-abline(h=0, col = "red")
-plot(X5, t, col = "blue", xlab = "x5", ylab = "Residuals", main = "Residuals vs x5")
-abline(h=0, col = "red")
-plot(X6, t, col = "blue", xlab = "x6", ylab = "Residuals", main = "Residuals vs x6")
-abline(h=0, col = "red")
-plot(X7, t, col = "blue", xlab = "x7", ylab = "Residuals", main = "Residuals vs x7")
-abline(h=0, col = "red")
-plot(X8, t, col = "blue", xlab = "x8", ylab = "Residuals", main = "Residuals vs x8")
-abline(h=0, col = "red")
-plot(X9, t, col = "blue", xlab = "x9", ylab = "Residuals", main = "Residuals vs x9")
-abline(h=0, col = "red")
-plot(X10, t, col = "blue", xlab = "x10", ylab = "Residuals", main = "Residuals vs x10")
-abline(h=0, col = "red")
-plot(X11, t, col = "blue", xlab = "x11", ylab = "Residuals", main = "Residuals vs x11")
-abline(h=0, col = "red")
-plot(X12, t, col = "blue", xlab = "x12", ylab = "Residuals", main = "Residuals vs x12")
-abline(h=0, col = "red")
+# plot(X1, t, col = "blue", xlab = "x1", ylab = "Residuals", main = "Residuals vs x1")
+# abline(h=0, col = "red")
+# plot(X2, t, col = "blue", xlab = "x2", ylab = "Residuals", main = "Residuals vs x2")
+# abline(h=0, col = "red")
+# plot(X3, t, col = "blue", xlab = "x3", ylab = "Residuals", main = "Residuals vs x3")
+# abline(h=0, col = "red")
+# plot(X4, t, col = "blue", xlab = "x4", ylab = "Residuals", main = "Residuals vs x4")
+# abline(h=0, col = "red")
+# plot(X5, t, col = "blue", xlab = "x5", ylab = "Residuals", main = "Residuals vs x5")
+# abline(h=0, col = "red")
+# plot(X6, t, col = "blue", xlab = "x6", ylab = "Residuals", main = "Residuals vs x6")
+# abline(h=0, col = "red")
+# plot(X7, t, col = "blue", xlab = "x7", ylab = "Residuals", main = "Residuals vs x7")
+# abline(h=0, col = "red")
+# plot(X8, t, col = "blue", xlab = "x8", ylab = "Residuals", main = "Residuals vs x8")
+# abline(h=0, col = "red")
+# plot(X9, t, col = "blue", xlab = "x9", ylab = "Residuals", main = "Residuals vs x9")
+# abline(h=0, col = "red")
+# plot(X10, t, col = "blue", xlab = "x10", ylab = "Residuals", main = "Residuals vs x10")
+# abline(h=0, col = "red")
+# plot(X11, t, col = "blue", xlab = "x11", ylab = "Residuals", main = "Residuals vs x11")
+# abline(h=0, col = "red")
+# plot(X12, t, col = "blue", xlab = "x12", ylab = "Residuals", main = "Residuals vs x12")
+# abline(h=0, col = "red")
 
 # Partial Regression Plots 
 avPlots(model)
-
-# Partial Residual Plots
-crPlots(model)
+# 
+# # Partial Residual Plots
+# crPlots(model)
 
 
 
