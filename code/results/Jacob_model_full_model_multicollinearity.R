ac073f5817d2449143aad49b4f1f4f1bfb873b90
Jacob Winch <winch@ualberta.ca>
2023-12-06 19:05:59 -0700

    Github Organization

---
+++ code/results/Jacob_model_full_model_multicollinearity.R
@@ -0,0 +1,48 @@
+library(olsrr)
+library(lmtest)
+library(MASS)
+library(car)
+data <- read.table("data/data_estimation_random.txt", header = FALSE)
+
+y <- data$V11
+x1 <- data$V2
+x2 <- data$V3
+x3 <- data$V4
+x4 <- data$V5
+x5 <- data$V6
+x6 <- data$V7 
+x7 <- data$V8
+x8 <- data$V9
+x9 <- data$V10
+x10 <- ifelse(data$V12 == 1, 1, 0)
+x11 <- ifelse(data$V12 == 2, 1, 0)
+x12 <- ifelse(data$V12 == 3, 1, 0)
+
+transformed_y <- 1/log(y)
+transformed_x1 <- 1/sqrt(x1)
+transformed_x2 <- sqrt(x2)
+transformed_x3 <- x3
+transformed_x4 <- log(x4)
+transformed_x5 <- log(x5)
+transformed_x6 <- sqrt(x6)
+transformed_x7 <- x7
+transformed_x8 <- 1/log(x8)
+transformed_x9 <- sqrt(x9)
+transformed_x10 <- x10
+transformed_x11 <- x11
+transformed_x12 <- x12
+
+final_transformed_model = lm(transformed_y ~ transformed_x1 + transformed_x2 +
+                               transformed_x4 + transformed_x5 + 
+                               transformed_x7 + transformed_x8 +
+                               transformed_x10+ transformed_x9 +
+                               transformed_x11 + 
+                               transformed_x10 * transformed_x2 +
+                               transformed_x10 * transformed_x1 +
+                               transformed_x11 * transformed_x7 +
+                               transformed_x10 * transformed_x5 +
+                               transformed_x10 * transformed_x4)
+
+ols_coll_diag(final_transformed_model)
+
+
